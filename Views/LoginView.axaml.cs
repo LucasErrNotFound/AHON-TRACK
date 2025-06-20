b7e27ebcc32ba266f6f467e7e29cff646fc49c24
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-06-20 17:15:45 +0800

    fix: slight public constructor naming scheme error by replacing MainWindow into LoginView

--- Views/LoginView.axaml.cs
+++ Views/LoginView.axaml.cs
@@ -1,10 +1,11 @@
 using Avalonia.Controls;
+using System;
 
 namespace AHON_TRACK.Views
 {
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
