631a773d089066a25ba920405ca8abe4bb2fbbd7
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-06-20 17:22:52 +0800

    feat: add the view and viewmodel for the MainWindow that houses all the key modules in the future

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShadUI.Dialogs;
+using ShadUI.Toasts;
+using System;
+
+namespace AHON_TRACK.ViewModels
+{
+    public partial class MainWindowViewModel(DialogManager dialogManager) : ViewModelBase
+    {
+        public MainWindowViewModel() : this(Design.IsDesignMode ? new DialogManager() : null!)
+        {
+        }
+
+        [ObservableProperty]
+        private DialogManager _dialogManager = dialogManager;
+
+        [RelayCommand]
+        private void TryClose()
+        {
+            DialogManager.CreateDialog("Close", "Do you really want to exit?")
+                .WithPrimaryButton("Yes", OnAcceptExit)
+                .WithCancelButton("No")
+                .WithMinWidth(300)
+                .Show();
+        }
+
+        private void OnAcceptExit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,38 @@
+using AHON_TRACK.ViewModels;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Window = ShadUI.Controls.Window;
+
+namespace AHON_TRACK.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Closing += OnClosing;
+        //FullscreenButton.Click += OnFullScreen;
+        //ExitFullscreenButton.Click += OnExitFullScreen;
+    }
+
+    private void OnFullScreen(object? sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.FullScreen;
+    }
+
+    private void OnExitFullScreen(object? sender, RoutedEventArgs e)
+    {
+        ExitFullScreen();
+    }
+
+    private void OnClosing(object? sender, WindowClosingEventArgs e)
+    {
+        e.Cancel = true;
+
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            viewModel.TryCloseCommand.Execute(null);
+        }
+    }
+}
