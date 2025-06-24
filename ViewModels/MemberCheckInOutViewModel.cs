03044ffbea542c41a1c8880a4ad332aae285229a
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-06-24 16:49:36 +0800

    feat: add ViewModels to the rest of the modules

---
+++ ViewModels/MemberCheckInOutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHON_TRACK.ViewModels
+{
+    public partial class MemberCheckInOutViewModel : ViewModelBase
+    {
+        public void Initialize()
+        {
+        }
+    }
+}
