6579ae8d1b01f51d430deff08cb9a998af6a1a5a
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-07-01 17:06:25 +0800

    chore: add functionality where Date and Int type can be converted to String and Bool (vice-versa)

---
+++ Functionalities/DateToStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace AHON_TRACK.Functionalities;
+
+public class DateToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime date)
+        {
+            return date.ToString("MMMM d, yyyy");
+        }
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string dateString && !string.IsNullOrWhiteSpace(dateString))
+        {
+            if (DateTime.TryParseExact(dateString, "MMMM d, yyyy", culture, DateTimeStyles.None, out DateTime exactResult))
+            {
+                return exactResult;
+            }
+            if (DateTime.TryParse(dateString, culture, DateTimeStyles.None, out DateTime generalResult))
+            {
+                return generalResult;
+            }
+        }
+        return DateTime.Today;
+    }
+}
