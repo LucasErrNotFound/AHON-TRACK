11e6e1378143f7672fda5ac9bfa2107f06bb5125
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-07-07 21:24:28 +0800

    feat: add functionality to Add New Employee

---
+++ Components/EmployeeDetails/EmployeeDetailsDialogCard.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AHON_TRACK.Components.EmployeeDetails;
+
+public partial class EmployeeDetailsDialogCard : UserControl
+{
+    public EmployeeDetailsDialogCard()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Components/ViewModels/EmployeeDetailsDialogCardViewModel.cs
@@ -0,0 +1,42 @@
+﻿using AHON_TRACK.ViewModels;
+using CommunityToolkit.Mvvm.Input;
+using HotAvalonia;
+using ShadUI;
+
+namespace AHON_TRACK.Components.ViewModels;
+
+public sealed partial class EmployeeDetailsDialogCardViewModel : ViewModelBase
+{
+    private readonly DialogManager _dialogManager;
+    public EmployeeDetailsDialogCardViewModel(DialogManager dialogManager)
+    {
+        _dialogManager = dialogManager;
+    }
+
+    public EmployeeDetailsDialogCardViewModel()
+    {
+        _dialogManager = new DialogManager();
+    }
+
+    [AvaloniaHotReload]
+    public void Initialize()
+    {
+        ClearAllErrors();
+    }
+
+    [RelayCommand]
+    private void SaveDetails()
+    {
+        ClearAllErrors();
+        ValidateAllProperties();
+
+        if (HasErrors) return;
+        _dialogManager.Close(this, new CloseDialogOptions { Success = true });
+    }
+
+    [RelayCommand]
+    private void Cancel()
+    {
+        _dialogManager.Close(this);
+    }
+}
