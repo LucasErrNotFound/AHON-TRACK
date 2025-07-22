cc6747c158f1b7847937df9a9d3423e7cc92d2fe
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-07-22 17:28:24 +0800

    feat(walk-in-purchase): add purchase page for logging walk ins

---
+++ Components/LogWalkInPurchase/LogWalkInPurchaseView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AHON_TRACK.Components.LogWalkInPurchase;
+
+public partial class LogWalkInPurchaseView : UserControl
+{
+    public LogWalkInPurchaseView()
+    {
+        InitializeComponent();
+    }
+}
