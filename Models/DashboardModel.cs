2d5f0cbb1b5abae2a04141593a9c2dc9c0a64bc3
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-07-25 15:19:22 +0800

    refactor(dashboard-model): improve List, remove unnecessary code, etc. Revamp for ReSharper

--- Models/DashboardModel.cs
+++ Models/DashboardModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AHON_TRACK.Models;
@@ -12,7 +11,7 @@ public class SalesItem
     public string CustomerName { get; set; } = string.Empty;
     public string CustomerType { get; set; } = "Gym Member";
     public string ProductName { get; set; } = string.Empty;
-    public decimal Amount { get; set; }
+    public decimal Amount { get; init; }
     public string AvatarSource { get; set; } = "avares://AHON_TRACK/Assets/MainWindowView/user.png";
 
     // Formatted currency for display
@@ -28,7 +27,7 @@ public class TrainingSession
     public string Location { get; set; } = string.Empty;
     public string TimeSlot { get; set; } = string.Empty;
     public string AvatarSource { get; set; } = "avares://AHON_TRACK/Assets/MainWindowView/user.png";
-    public DateTime Date { get; set; }
+    public DateTime Date { get; init; }
 
     // Formatted date for display
     public string FormattedDate => Date.ToString("MMM dd, yyyy");
@@ -47,9 +46,9 @@ public class RecentLog
 // Main Dashboard Model - handles all data operations
 public class DashboardModel
 {
-    // Constants for year range || JUST A PROTOTYPE CHILL OUT || I did this so that we can see the year items on the dropmenu
-    private const int START_YEAR = 2025;
-    private const int YEARS_TO_SHOW = 10;
+    // Constants for year range || JUST A PROTOTYPE CHILL OUT || I did this so that we can see the year items on the drop-menu
+    private const int StartYear = 2025;
+    private const int YearsToShow = 10;
 
     // Dictionary to store data for different years
     private readonly Dictionary<int, int[]> _yearlyData = new();
@@ -63,17 +62,17 @@ public class DashboardModel
 
     public List<SalesItem> GetSampleSalesData()
     {
-        return new List<SalesItem>
-            {
-                new SalesItem { CustomerName = "Jedd Calubayan", ProductName = "Red Horse Mucho", Amount = 300.00m },
-                new SalesItem { CustomerName = "Sianrey Flora", ProductName = "Membership Renewal", Amount = 500.00m },
-                new SalesItem { CustomerName = "JC Casidor", ProductName = "Protein Milk Shake", Amount = 35.00m },
-                new SalesItem { CustomerName = "Mardie Dela Cruz", ProductName = "AHON T-Shirt", Amount = 135.00m },
-                new SalesItem { CustomerName = "JL Taberdo", ProductName = "Lifting Straps", Amount = 360.00m },
-                new SalesItem { CustomerName = "Jav Agustin", ProductName = "AHON Tumbler", Amount = 235.00m },
-                new SalesItem { CustomerName = "Marc Torres", ProductName = "Gym Membership", Amount = 499.00m },
-                new SalesItem { CustomerName = "Maverick Lim", ProductName = "Cobra Berry", Amount = 40.00m }
-            };
+        return
+        [
+            new SalesItem { CustomerName = "Jedd Calubayan", ProductName = "Red Horse Mucho", Amount = 300.00m },
+            new SalesItem { CustomerName = "Sianrey Flora", ProductName = "Membership Renewal", Amount = 500.00m },
+            new SalesItem { CustomerName = "JC Casidor", ProductName = "Protein Milk Shake", Amount = 35.00m },
+            new SalesItem { CustomerName = "Mardie Dela Cruz", ProductName = "AHON T-Shirt", Amount = 135.00m },
+            new SalesItem { CustomerName = "JL Taberdo", ProductName = "Lifting Straps", Amount = 360.00m },
+            new SalesItem { CustomerName = "Jav Agustin", ProductName = "AHON Tumbler", Amount = 235.00m },
+            new SalesItem { CustomerName = "Marc Torres", ProductName = "Gym Membership", Amount = 499.00m },
+            new SalesItem { CustomerName = "Maverick Lim", ProductName = "Cobra Berry", Amount = 40.00m }
+        ];
     }
 
     public async Task<List<SalesItem>> GetSalesFromDatabaseAsync()
@@ -85,7 +84,7 @@ public class DashboardModel
         // return sales.ToList();
 
         await Task.Delay(100); // Simulate async operation
-        return new List<SalesItem>();
+        return [];
     }
 
     public string GenerateSalesSummary(int salesCount)
@@ -99,90 +98,99 @@ public class DashboardModel
 
     public List<TrainingSession> GetSampleTrainingSessionsData()
     {
-        return new List<TrainingSession>
+        return
+        [
+            new TrainingSession
             {
-                new TrainingSession
-                {
-                    ClientName = "Rome Jedd Calubayan",
-                    MembershipType = "Gym Member",
-                    TrainingType = "Boxing",
-                    Location = "Boxing Ring",
-                    TimeSlot = "8:00AM - 9:00AM",
-                    Date = new DateTime(2025, 6, 16)
-                },
-                new TrainingSession
-                {
-                    ClientName = "JC Casidore",
-                    MembershipType = "Gym Member",
-                    TrainingType = "Boxing",
-                    Location = "Boxing Ring",
-                    TimeSlot = "8:00AM - 9:00AM",
-                    Date = new DateTime(2025, 6, 16)
-                },
-                new TrainingSession
-                {
-                    ClientName = "Mardie Dela Cruz",
-                    MembershipType = "Walk-in",
-                    TrainingType = "Muay Thai",
-                    Location = "Gym Mat",
-                    TimeSlot = "1:00PM - 2:00PM",
-                    Date = new DateTime(2025, 6, 17)
-                },
-                new TrainingSession
-                {
-                    ClientName = "JL Taberdo",
-                    MembershipType = "Gym Member",
-                    TrainingType = "Crossfit Training",
-                    Location = "Gym Mat",
-                    TimeSlot = "4:00PM - 5:00PM",
-                    Date = new DateTime(2025, 6, 19)
-                },
-                new TrainingSession
-                {
-                    ClientName = "Jav Agustin",
-                    MembershipType = "Walk-in",
-                    TrainingType = "Muay Thai",
-                    Location = "Gym Mat",
-                    TimeSlot = "1:00PM - 2:00PM",
-                    Date = new DateTime(2025, 6, 21)
-                },
-                new TrainingSession
-                {
-                    ClientName = "Sianrey Flora",
-                    MembershipType = "Gym Member",
-                    TrainingType = "Boxing",
-                    Location = "Boxing Ring",
-                    TimeSlot = "2:00PM - 5:00PM",
-                    Date = new DateTime(2025, 6, 26)
-                },
-                new TrainingSession
-                {
-                    ClientName = "Marc Torres",
-                    MembershipType = "Gym Member",
-                    TrainingType = "Boxing",
-                    Location = "Boxing Ring",
-                    TimeSlot = "3:00PM - 6:00PM",
-                    Date = new DateTime(2025, 6, 27)
-                },
-                new TrainingSession
-                {
-                    ClientName = "Maverick Lim",
-                    MembershipType = "Gym Member",
-                    TrainingType = "Crossift Training",
-                    Location = "Gym Mat",
-                    TimeSlot = "11:00AM - 12:00PM",
-                    Date = new DateTime(2025, 6, 28)
-                },
-                new TrainingSession
-                {
-                    ClientName = "Dave Dapitillo",
-                    MembershipType = "Walk-in",
-                    TrainingType = "Muay Thai",
-                    Location = "Gym Mat",
-                    TimeSlot = "12:00PM - 2:00PM",
-                    Date = new DateTime(2025, 6, 28)
-                },
-            };
+                ClientName = "Rome Jedd Calubayan",
+                MembershipType = "Gym Member",
+                TrainingType = "Boxing",
+                Location = "Boxing Ring",
+                TimeSlot = "8:00AM - 9:00AM",
+                Date = new DateTime(2025, 6, 16)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "JC Casidore",
+                MembershipType = "Gym Member",
+                TrainingType = "Boxing",
+                Location = "Boxing Ring",
+                TimeSlot = "8:00AM - 9:00AM",
+                Date = new DateTime(2025, 6, 16)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "Mardie Dela Cruz",
+                MembershipType = "Walk-in",
+                TrainingType = "Muay Thai",
+                Location = "Gym Mat",
+                TimeSlot = "1:00PM - 2:00PM",
+                Date = new DateTime(2025, 6, 17)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "JL Taberdo",
+                MembershipType = "Gym Member",
+                TrainingType = "Crossfit Training",
+                Location = "Gym Mat",
+                TimeSlot = "4:00PM - 5:00PM",
+                Date = new DateTime(2025, 6, 19)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "Jav Agustin",
+                MembershipType = "Walk-in",
+                TrainingType = "Muay Thai",
+                Location = "Gym Mat",
+                TimeSlot = "1:00PM - 2:00PM",
+                Date = new DateTime(2025, 6, 21)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "Sianrey Flora",
+                MembershipType = "Gym Member",
+                TrainingType = "Boxing",
+                Location = "Boxing Ring",
+                TimeSlot = "2:00PM - 5:00PM",
+                Date = new DateTime(2025, 6, 26)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "Marc Torres",
+                MembershipType = "Gym Member",
+                TrainingType = "Boxing",
+                Location = "Boxing Ring",
+                TimeSlot = "3:00PM - 6:00PM",
+                Date = new DateTime(2025, 6, 27)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "Maverick Lim",
+                MembershipType = "Gym Member",
+                TrainingType = "CrossFit Training",
+                Location = "Gym Mat",
+                TimeSlot = "11:00AM - 12:00PM",
+                Date = new DateTime(2025, 6, 28)
+            },
+
+            new TrainingSession
+            {
+                ClientName = "Dave Dapitillo",
+                MembershipType = "Walk-in",
+                TrainingType = "Muay Thai",
+                Location = "Gym Mat",
+                TimeSlot = "12:00PM - 2:00PM",
+                Date = new DateTime(2025, 6, 28)
+            }
+
+        ];
     }
 
     public async Task<List<TrainingSession>> GetTrainingSessionsFromDatabaseAsync()
@@ -196,7 +204,7 @@ public class DashboardModel
         // return sessions.ToList();
 
         await Task.Delay(100); // Simulate async operation
-        return new List<TrainingSession>();
+        return [];
     }
 
     public string GenerateTrainingSessionsSummary(int sessionCount)
@@ -206,7 +214,7 @@ public class DashboardModel
 
     public int FindInsertionIndex(List<TrainingSession> sessions, TrainingSession newSession)
     {
-        for (int i = 0; i < sessions.Count; i++)
+        for (var i = 0; i < sessions.Count; i++)
         {
             if (sessions[i].Date > newSession.Date)
             {
@@ -222,44 +230,48 @@ public class DashboardModel
 
     public List<RecentLog> GetSampleRecentLogsData()
     {
-        return new List<RecentLog>
+        return
+        [
+            new RecentLog
             {
-                new RecentLog
-                {
-                    Username = "Kuya Rome",
-                    UserType = "Gym Admin",
-                    ActionLogName = "Printed monthly reports",
-                    LogDateTime = DateTime.Now.AddMinutes(-30)
-                },
-                new RecentLog
-                {
-                    Username = "Jaycee",
-                    UserType = "Gym Admin",
-                    ActionLogName = "Modified rates in boxing package",
-                    LogDateTime = DateTime.Now.AddHours(-1)
-                },
-                new RecentLog
-                {
-                    Username = "Figora",
-                    UserType = "Gym Staff",
-                    ActionLogName = "Reported a broken gym equipment",
-                    LogDateTime = DateTime.Now.AddHours(-2)
-                },
-                new RecentLog
-                {
-                    Username = "JeyEL",
-                    UserType = "Gym Staff",
-                    ActionLogName = "Scheduled a gym equipment maintenance",
-                    LogDateTime = DateTime.Now.AddHours(-3)
-                },
-                new RecentLog
-                {
-                    Username = "Mr. Javitos",
-                    UserType = "Gym Admin",
-                    ActionLogName = "Printed month gym member summary",
-                    LogDateTime = DateTime.Now.AddHours(-4)
-                }
-            };
+                Username = "Kuya Rome",
+                UserType = "Gym Admin",
+                ActionLogName = "Printed monthly reports",
+                LogDateTime = DateTime.Now.AddMinutes(-30)
+            },
+
+            new RecentLog
+            {
+                Username = "Jaycee",
+                UserType = "Gym Admin",
+                ActionLogName = "Modified rates in boxing package",
+                LogDateTime = DateTime.Now.AddHours(-1)
+            },
+
+            new RecentLog
+            {
+                Username = "Figora",
+                UserType = "Gym Staff",
+                ActionLogName = "Reported a broken gym equipment",
+                LogDateTime = DateTime.Now.AddHours(-2)
+            },
+
+            new RecentLog
+            {
+                Username = "JeyEL",
+                UserType = "Gym Staff",
+                ActionLogName = "Scheduled a gym equipment maintenance",
+                LogDateTime = DateTime.Now.AddHours(-3)
+            },
+
+            new RecentLog
+            {
+                Username = "Mr. Javitos",
+                UserType = "Gym Admin",
+                ActionLogName = "Printed month gym member summary",
+                LogDateTime = DateTime.Now.AddHours(-4)
+            }
+        ];
     }
 
     public async Task<List<RecentLog>> GetRecentLogsFromDatabaseAsync()
@@ -272,7 +284,7 @@ public class DashboardModel
         // return logs.ToList();
 
         await Task.Delay(100); // Simulate async operation
-        return new List<RecentLog>();
+        return [];
     }
 
     public string GenerateRecentLogsSummary(int logCount) => $"You have {logCount} recent action logs today";
@@ -284,10 +296,10 @@ public class DashboardModel
     public List<int> GetAvailableYears()
     {
         var currentYear = DateTime.Now.Year;
-        var endYear = Math.Max(currentYear + 5, START_YEAR + YEARS_TO_SHOW);
+        var endYear = Math.Max(currentYear + 5, StartYear + YearsToShow);
 
         var years = new List<int>();
-        for (int year = START_YEAR; year <= endYear; year++)
+        for (var year = StartYear; year <= endYear; year++)
         {
             years.Add(year);
         }
@@ -322,7 +334,7 @@ public class DashboardModel
         // Generate sample data based on the year
         // Replace this with actual data loading logic
         var baseValues = new[] { 12000, 15125, 14200, 20000, 14000, 13500, 15000, 19000, 10000, 19930, 13000, 26000 };
-        var yearMultiplier = 1 + (year - START_YEAR) * 0.1; // 10% increase per year
+        var yearMultiplier = 1 + (year - StartYear) * 0.1; // 10% increase per year
 
         return [.. baseValues.Select(value => (int)(value * yearMultiplier))];
     }
