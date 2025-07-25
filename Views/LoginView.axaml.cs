714e6d40df825542f9a4ed3553e4e519ec69d4d8
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-07-25 14:16:54 +0800

    refactor(login-view-cs): remove unnecessary use of dependencies

--- Views/LoginView.axaml.cs
+++ Views/LoginView.axaml.cs
@@ -1,9 +1,6 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 using Window = ShadUI.Window;
 
-namespace AHON_TRACK;
+namespace AHON_TRACK.Views;
 
 public partial class LoginView : Window
 {
