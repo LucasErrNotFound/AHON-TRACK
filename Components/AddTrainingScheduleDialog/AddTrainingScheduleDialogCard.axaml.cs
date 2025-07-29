99f31bd93de810280b8c19e8d24675003b570e8a
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-07-29 22:23:42 +0800

    feat(components): add custom dialog components for adding training schedules and setting its view model

---
+++ Components/AddTrainingScheduleDialog/AddTrainingScheduleDialogCard.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AHON_TRACK.Components.AddTrainingScheduleDialog;
+
+public partial class AddTrainingScheduleDialogCard : UserControl
+{
+    public AddTrainingScheduleDialogCard()
+    {
+        InitializeComponent();
+    }
+}
