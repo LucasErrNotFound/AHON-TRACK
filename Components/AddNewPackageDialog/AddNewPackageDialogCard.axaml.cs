04621adc0b34d5999a24328195fd6340c25f32dd
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-08-10 21:47:31 +0800

    feat: add the custom dialog; adding new package, to project

---
+++ Components/AddNewPackageDialog/AddNewPackageDialogCard.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AHON_TRACK.Components.AddNewPackageDialog;
+
+public partial class AddNewPackageDialogCard : UserControl
+{
+    public AddNewPackageDialogCard()
+    {
+        InitializeComponent();
+    }
+}
