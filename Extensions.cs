5f924af6c8a2f71401c21376a5e97c8e72e1eead
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-08-21 23:57:23 +0800

    feat(extensions): register equipment-dialog view and its viewmodel

--- Extensions.cs
+++ Extensions.cs
@@ -3,6 +3,7 @@ using AHON_TRACK.Components.AddNewEmployeeDialog;
 using AHON_TRACK.Components.AddNewPackageDialog;
 using AHON_TRACK.Components.AddTrainingScheduleDialog;
 using AHON_TRACK.Components.EditPackageDialog;
+using AHON_TRACK.Components.EquipmentDialog;
 using AHON_TRACK.Components.LogGymMemberDialog;
 using ShadUI;
 
@@ -18,6 +19,7 @@ public static class Extensions
         dialogService.Register<AddTrainingScheduleDialogCard, AddTrainingScheduleDialogCardViewModel>();
         dialogService.Register<AddNewPackageDialogCard, AddNewPackageDialogCardViewModel>();
         dialogService.Register<EditPackageDialogCard, EditPackageDialogCardViewModel>();
+        dialogService.Register<EquipmentDialogView, EquipmentDialogViewModel>();
 
         return service;
     }
