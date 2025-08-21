6535f8cc384543cac26ccd167f87279d293de5c7
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-08-22 00:00:15 +0800

    feat(equipment-inventory): invoke the custom equipment dialog to a button

--- ViewModels/EquipmentInventoryViewModel.cs
+++ ViewModels/EquipmentInventoryViewModel.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel;
+using AHON_TRACK.Components.ViewModels;
+using CommunityToolkit.Mvvm.Input;
 using HotAvalonia;
 using ShadUI;
 
@@ -10,12 +12,14 @@ public sealed partial class EquipmentInventoryViewModel : ViewModelBase, INaviga
     private readonly DialogManager _dialogManager;
     private readonly ToastManager _toastManager;
     private readonly PageManager _pageManager;
+    private readonly EquipmentDialogViewModel _equipmentDialogViewModel;
     
-    public EquipmentInventoryViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager)
+    public EquipmentInventoryViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager, EquipmentDialogViewModel equipmentDialogViewModel)
     {
         _dialogManager = dialogManager;
         _toastManager = toastManager;
         _pageManager = pageManager;
+        _equipmentDialogViewModel = equipmentDialogViewModel;
     }
     
     public EquipmentInventoryViewModel()
@@ -23,10 +27,30 @@ public sealed partial class EquipmentInventoryViewModel : ViewModelBase, INaviga
         _dialogManager = new DialogManager();
         _toastManager = new ToastManager();
         _pageManager = new PageManager(new ServiceProvider());
+        _equipmentDialogViewModel = new EquipmentDialogViewModel();
     }
 
     [AvaloniaHotReload]
     public void Initialize()
     {
+    }
+
+    [RelayCommand]
+    private void ShowAddEquipmentDialog()
+    {
+        _equipmentDialogViewModel.Initialize();
+        _dialogManager.CreateDialog(_equipmentDialogViewModel)
+            .WithSuccessCallback(_ =>
+                _toastManager.CreateToast("Added a new equipment")
+                    .WithContent($"You just added a new equipment to the database!")
+                    .DismissOnClick()
+                    .ShowSuccess())
+            .WithCancelCallback(() =>
+                _toastManager.CreateToast("Adding new equipment cancelled")
+                    .WithContent("If you want to add a new equipment, please try again.")
+                    .DismissOnClick()
+                    .ShowWarning()).WithMaxWidth(650)
+            .Dismissible()
+            .Show();
     }
 }
