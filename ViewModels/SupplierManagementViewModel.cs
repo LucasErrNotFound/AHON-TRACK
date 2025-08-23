216b24005c801ba99b40697d112cdd15058eb6e2
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-08-23 10:45:15 +0800

    feat(supplier-management): implement function command to show custom supplier-dialog

--- ViewModels/SupplierManagementViewModel.cs
+++ ViewModels/SupplierManagementViewModel.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel;
+using AHON_TRACK.Components.ViewModels;
+using CommunityToolkit.Mvvm.Input;
 using HotAvalonia;
 using ShadUI;
 
@@ -10,12 +12,14 @@ public sealed partial class SupplierManagementViewModel : ViewModelBase, INaviga
     private readonly DialogManager _dialogManager;
     private readonly ToastManager _toastManager;
     private readonly PageManager _pageManager;
+    private readonly SupplierDialogCardViewModel _supplierDialogCardViewModel;
 
-    public SupplierManagementViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager)
+    public SupplierManagementViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager,  SupplierDialogCardViewModel supplierDialogCardViewModel)
     {
         _dialogManager = dialogManager;
         _toastManager = toastManager;
         _pageManager = pageManager;
+        _supplierDialogCardViewModel = supplierDialogCardViewModel;
     }
 
     public SupplierManagementViewModel()
@@ -23,10 +27,30 @@ public sealed partial class SupplierManagementViewModel : ViewModelBase, INaviga
         _dialogManager = new DialogManager();
         _toastManager = new ToastManager();
         _pageManager = new PageManager(new ServiceProvider());
+        _supplierDialogCardViewModel = new SupplierDialogCardViewModel();
     }
 
     [AvaloniaHotReload]
     public void Initialize()
     {
+    }
+
+    [RelayCommand]
+    private void ShowAddSupplierDialog()
+    {
+        _supplierDialogCardViewModel.Initialize();
+        _dialogManager.CreateDialog(_supplierDialogCardViewModel)
+            .WithSuccessCallback(_ =>
+                _toastManager.CreateToast("Added a new supplier contact")
+                    .WithContent($"You just added a new supplier contact to the database!")
+                    .DismissOnClick()
+                    .ShowSuccess())
+            .WithCancelCallback(() =>
+                _toastManager.CreateToast("Adding new supplier contact cancelled")
+                    .WithContent("If you want to add a new supplier contact, please try again.")
+                    .DismissOnClick()
+                    .ShowWarning()).WithMaxWidth(650)
+            .Dismissible()
+            .Show();
     }
 }
