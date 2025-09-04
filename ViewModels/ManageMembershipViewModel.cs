e7c3138ce0648c1f944ef183ce913f6d57c67f34
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-09-04 21:13:54 +0800

    feat(manage-membership): implement add-new-member page navigation

--- ViewModels/ManageMembershipViewModel.cs
+++ ViewModels/ManageMembershipViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using AHON_TRACK.Components.ViewModels;
+using CommunityToolkit.Mvvm.Input;
 using HotAvalonia;
 using ShadUI;
 
@@ -10,12 +12,16 @@ public sealed partial class ManageMembershipViewModel : ViewModelBase, INavigabl
 	private readonly DialogManager _dialogManager;
 	private readonly ToastManager _toastManager;
 	private readonly PageManager _pageManager;
+	private readonly MemberDialogCardViewModel  _memberDialogCardViewModel;
+	private readonly AddNewMemberViewModel _addNewMemberViewModel;
 
-	public ManageMembershipViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager)
+	public ManageMembershipViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager,  MemberDialogCardViewModel memberDialogCardViewModel, AddNewMemberViewModel addNewMemberViewModel)
 	{
 		_dialogManager = dialogManager;
 		_toastManager = toastManager;
 		_pageManager = pageManager;
+		_memberDialogCardViewModel = memberDialogCardViewModel;
+		_addNewMemberViewModel = addNewMemberViewModel;
 	}
 
 	public ManageMembershipViewModel()
@@ -23,6 +29,8 @@ public sealed partial class ManageMembershipViewModel : ViewModelBase, INavigabl
 		_dialogManager = new DialogManager();
 		_toastManager = new ToastManager();
 		_pageManager = new PageManager(new ServiceProvider());
+		_memberDialogCardViewModel = new MemberDialogCardViewModel();
+		_addNewMemberViewModel = new AddNewMemberViewModel();
 	}
 
 	[AvaloniaHotReload]
@@ -30,4 +38,62 @@ public sealed partial class ManageMembershipViewModel : ViewModelBase, INavigabl
 	{
 
 	}
+
+	[RelayCommand]
+	private void OpenAddNewMemberView()
+	{
+		_pageManager.Navigate<AddNewMemberViewModel>();
+	} 
+	
+	[RelayCommand]
+	private void OpenUpgradeMemberView()
+	{
+		_pageManager.Navigate<AddNewMemberViewModel>();
+	} 
+	
+	[RelayCommand]
+	private void OpenRenewMemberView()
+	{
+		_pageManager.Navigate<AddNewMemberViewModel>();
+	} 
+	
+	[RelayCommand]
+	private void ShowDeleteMember()
+	{
+		_dialogManager
+			.CreateDialog(
+				"Are you absolutely sure?",
+				"This action cannot be undone. This will permanently delete and remove this member's data from your server.")
+			.WithPrimaryButton("Continue",
+				() => _toastManager.CreateToast("Delete data")
+					.WithContent("Data deleted successfully!")
+					.DismissOnClick()
+					.ShowSuccess()
+				, DialogButtonStyle.Destructive)
+			.WithCancelButton("Cancel")
+			.WithMaxWidth(512)
+			.Dismissible()
+			.Show();
+	} 
+	
+	/*
+	[RelayCommand]
+	private void ShowMemberDialog()
+	{
+		_memberDialogCardViewModel.Initialize();
+		_dialogManager.CreateDialog(_memberDialogCardViewModel)
+			.WithSuccessCallback(_ =>
+				_toastManager.CreateToast("Added a new gym member")
+					.WithContent($"You just added a new gym member to the database!")
+					.DismissOnClick()
+					.ShowSuccess())
+			.WithCancelCallback(() =>
+				_toastManager.CreateToast("Adding new gym member cancelled")
+					.WithContent("If you want to add a new gym member, please try again.")
+					.DismissOnClick()
+					.ShowWarning()).WithMaxWidth(950)
+			.Dismissible()
+			.Show();
+	}
+	*/
 }
