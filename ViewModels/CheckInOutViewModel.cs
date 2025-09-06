ade097de66144bae65d80d6695a0a355c1841a21
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-09-06 10:50:26 +0800

    feat(checkin-checkout): implement stamp of check-out time, both for walk-ins and members

--- ViewModels/CheckInOutViewModel.cs
+++ ViewModels/CheckInOutViewModel.cs
@@ -76,101 +76,101 @@ public partial class CheckInOutViewModel : ViewModelBase, INotifyPropertyChanged
 			new WalkInPerson
 			{
 				ID = 1018, FirstName = "Rome", LastName = "Calubayan", Age = 21, ContactNumber = "09283374574",
-				PackageType = "Gym", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = DateTime.Now.AddDays(-1)
+				PackageType = "Gym", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = null
 			},
 			new WalkInPerson
 			{
 				ID = 1017, FirstName = "Dave", LastName = "Dapitillo", Age = 22, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = DateTime.Now.AddDays(-1)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = null
 			},
 			new WalkInPerson
 			{
 				ID = 1016, FirstName = "Sianrey", LastName = "Flora", Age = 20, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = DateTime.Now.AddDays(-1)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1015, FirstName = "JC", LastName = "Casidor", Age = 30, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = DateTime.Now.AddDays(-1)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-1), CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1014, FirstName = "Mark", LastName = "Dela Cruz", Age = 21, ContactNumber = "09123456789",
 				PackageType = "Muay Thai", CheckInTime = DateTime.Now.AddDays(-2),
-				CheckOutTime = DateTime.Now.AddDays(-2)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1013, FirstName = "Mardie", LastName = "Dela Cruz Jr.", Age = 21, ContactNumber = "09123456789",
 				PackageType = "CrossFit", CheckInTime = DateTime.Now.AddDays(-2),
-				CheckOutTime = DateTime.Now.AddDays(-2)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1012, FirstName = "Marc", LastName = "Torres", Age = 26, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-2), CheckOutTime = DateTime.Now.AddDays(-2)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-2), CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1011, FirstName = "Maverick", LastName = "Lim", Age = 21, ContactNumber = "09123456789",
 				PackageType = "CrossFit", CheckInTime = DateTime.Now.AddDays(-2),
-				CheckOutTime = DateTime.Now.AddDays(-2)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1010, FirstName = "Adriel", LastName = "Del Rosario", Age = 21, ContactNumber = "09123456789",
 				PackageType = "CrossFit", CheckInTime = DateTime.Now.AddDays(-3),
-				CheckOutTime = DateTime.Now.AddDays(-3)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1009, FirstName = "JL", LastName = "Taberdo", Age = 21, ContactNumber = "09123456789",
 				PackageType = "CrossFit", CheckInTime = DateTime.Now.AddDays(-3),
-				CheckOutTime = DateTime.Now.AddDays(-3)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1008, FirstName = "Jav", LastName = "Agustin", Age = 21, ContactNumber = "09123456789",
 				PackageType = "Muay Thai", CheckInTime = DateTime.Now.AddDays(-3),
-				CheckOutTime = DateTime.Now.AddDays(-3)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1007, FirstName = "Daniel", LastName = "Empinado", Age = 20, ContactNumber = "09123456789",
 				PackageType = "Muay Thai", CheckInTime = DateTime.Now.AddDays(-3),
-				CheckOutTime = DateTime.Now.AddDays(-3)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1006, FirstName = "Marion", LastName = "Dela Roca", Age = 20, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-3), CheckOutTime = DateTime.Now.AddDays(-3)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-3), CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1005, FirstName = "Sianrey", LastName = "Flora", Age = 20, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-4), CheckOutTime = DateTime.Now.AddDays(-4)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-4), CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1004, FirstName = "JC", LastName = "Casidor", Age = 30, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-4), CheckOutTime = DateTime.Now.AddDays(-4)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-4), CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1003, FirstName = "Mark", LastName = "Dela Cruz", Age = 21, ContactNumber = "09123456789",
 				PackageType = "Muay Thai", CheckInTime = DateTime.Now.AddDays(-4),
-				CheckOutTime = DateTime.Now.AddDays(-4)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1002, FirstName = "Mardie", LastName = "Dela Cruz Jr.", Age = 21, ContactNumber = "09123456789",
 				PackageType = "CrossFit", CheckInTime = DateTime.Now.AddDays(-4),
-				CheckOutTime = DateTime.Now.AddDays(-4)
+				CheckOutTime = null 
 			},
 			new WalkInPerson
 			{
 				ID = 1001, FirstName = "Marc", LastName = "Torres", Age = 26, ContactNumber = "09123456789",
-				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-4), CheckOutTime = DateTime.Now.AddDays(-4)
+				PackageType = "Boxing", CheckInTime = DateTime.Now.AddDays(-4), CheckOutTime = null 
 			}
 		];
 	}
@@ -184,37 +184,37 @@ public partial class CheckInOutViewModel : ViewModelBase, INotifyPropertyChanged
 				ID = 2006, MemberPicture = "avares://AHON_TRACK/Assets/MainWindowView/user-admin.png",
 				FirstName = "Mardie", LastName = "Dela Cruz", ContactNumber = "09123456789",
 				MembershipType = "Gym Member", Status = "Active", CheckInTime = DateTime.Now.AddHours(-1),
-				CheckOutTime = DateTime.Now.AddHours(-1)
+				CheckOutTime = null 
 			},
 			new MemberPerson
 			{
 				ID = 2005, MemberPicture = null, FirstName = "Cirilo", LastName = "Pagayunan Jr.",
 				ContactNumber = "09123456789", MembershipType = "Free Trial", Status = "Active",
-				CheckInTime = DateTime.Now.AddHours(-1), CheckOutTime = DateTime.Now.AddHours(-1)
+				CheckInTime = DateTime.Now.AddHours(-1), CheckOutTime = null 
 			},
 			new MemberPerson
 			{
 				ID = 2004, MemberPicture = null, FirstName = "Raymart", LastName = "Soneja",
 				ContactNumber = "09123456789", MembershipType = "Gym Member", Status = "Inactive",
-				CheckInTime = DateTime.Now.AddDays(-2), CheckOutTime = DateTime.Now.AddDays(-2)
+				CheckInTime = DateTime.Now.AddDays(-2), CheckOutTime = null 
 			},
 			new MemberPerson
 			{
 				ID = 2003, MemberPicture = null, FirstName = "Xyrus", LastName = "Jawili",
 				ContactNumber = "09123456789", MembershipType = "Gym Member", Status = "Inactive",
-				CheckInTime = DateTime.Now.AddDays(-2), CheckOutTime = DateTime.Now.AddDays(-2)
+				CheckInTime = DateTime.Now.AddDays(-2), CheckOutTime = null 
 			},
 			new MemberPerson
 			{
 				ID = 2002, MemberPicture = null, FirstName = "Nash", LastName = "Floralde",
 				ContactNumber = "09123456789", MembershipType = "Free Trial", Status = "Terminated",
-				CheckInTime = DateTime.Now.AddDays(-3), CheckOutTime = DateTime.Now.AddDays(-3)
+				CheckInTime = DateTime.Now.AddDays(-3), CheckOutTime = null 
 			},
 			new MemberPerson
 			{
 				ID = 2001, MemberPicture = null, FirstName = "Ry", LastName = "Estrada", ContactNumber = "09123456789",
 				MembershipType = "Free Trial", Status = "Terminated", CheckInTime = DateTime.Now.AddDays(-3),
-				CheckOutTime = DateTime.Now.AddDays(-3)
+				CheckOutTime = null 
 			}
 		];
 	}
@@ -235,6 +235,30 @@ public partial class CheckInOutViewModel : ViewModelBase, INotifyPropertyChanged
 					.DismissOnClick()
 					.ShowWarning()).WithMaxWidth(1000)
 			.Show();
+	}
+
+	[RelayCommand]
+	private void StampWalkInCheckOut(WalkInPerson? walkIn)
+	{
+		if (walkIn is null) return;
+		walkIn.CheckOutTime = DateTime.Now;
+		
+		_toastManager.CreateToast("Stamp Check Out")
+			.WithContent($"Stamped {walkIn?.FirstName} {walkIn?.LastName} Check-out time!")
+			.DismissOnClick()
+			.ShowSuccess();
+	}
+	
+	[RelayCommand]
+	private void StampMemberInCheckOut(MemberPerson? member)
+	{
+		if (member is null) return;
+		member.CheckOutTime = DateTime.Now;
+		
+		_toastManager.CreateToast("Stamp Check Out")
+			.WithContent($"Stamped {member?.FirstName} {member?.LastName} Check-out time!")
+			.DismissOnClick()
+			.ShowSuccess();
 	}
 
 	[RelayCommand]
@@ -260,7 +284,7 @@ public partial class CheckInOutViewModel : ViewModelBase, INotifyPropertyChanged
 	}
 }
 
-public class WalkInPerson
+public class WalkInPerson : ObservableObject
 {
 	public int ID { get; set; }
 	public string FirstName { get; set; } = string.Empty;
@@ -269,7 +293,14 @@ public class WalkInPerson
 	public string ContactNumber { get; set; } = string.Empty;
 	public string PackageType { get; set; } = string.Empty;
 	public DateTime? CheckInTime { get; set; }
-	public DateTime? CheckOutTime { get; set; }
+	private DateTime? _checkOutTime;
+
+	public DateTime? CheckOutTime
+	{
+		get =>  _checkOutTime;
+		set => SetProperty(ref _checkOutTime, value);
+	}
+	
 	public string DateFormatted => CheckInTime?.ToString("MMMM dd, yyyy") ?? string.Empty;
 }
 
@@ -283,7 +314,14 @@ public class MemberPerson : ViewModelBase
 	public string MembershipType { get; set; } = string.Empty;
 	public string Status { get; set; } = string.Empty;
 	public DateTime? CheckInTime { get; set; }
-	public DateTime? CheckOutTime { get; set; }
+	private DateTime? _checkOutTime;
+
+	public DateTime? CheckOutTime
+	{
+		get => _checkOutTime;
+		set => SetProperty(ref _checkOutTime, value);
+	}
+	
 	public string DateFormatted => CheckInTime?.ToString("MMMM dd, yyyy") ?? string.Empty;
 	public string MemberPicturePath => string.IsNullOrEmpty(MemberPicture) || MemberPicture == "null"
 		? "avares://AHON_TRACK/Assets/MainWindowView/user.png"
