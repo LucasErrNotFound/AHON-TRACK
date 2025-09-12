69fa23ab1a33332ebf68f03a2139e0ba3224397b
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-09-12 19:52:35 +0800

    feat(training-schedules): implement a date filtering functionality to handle different schedule dates per data

--- ViewModels/TrainingSchedulesViewModel.cs
+++ ViewModels/TrainingSchedulesViewModel.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using AHON_TRACK.Components.ViewModels;
-using Avalonia.Collections;
 using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -23,7 +25,25 @@ public sealed partial class TrainingSchedulesViewModel : ViewModelBase, INavigab
     private DateTime _selectedDate = DateTime.Today;
     
     [ObservableProperty]
-    private DataGridCollectionView _scheduledClients;
+    private List<ScheduledPerson> _originalScheduledPeople = [];
+    
+    [ObservableProperty]
+    private List<ScheduledPerson> _currentScheduledPeople = [];
+    
+    [ObservableProperty]
+    private bool _selectAll;
+
+    [ObservableProperty]
+    private int _selectedCount;
+    
+    [ObservableProperty]
+    private int _totalCount;
+    
+    [ObservableProperty]
+    private bool _isInitialized;
+
+    [ObservableProperty]
+    private ObservableCollection<ScheduledPerson> _scheduledPeople = [];
     
     private readonly DialogManager _dialogManager;
     private readonly ToastManager _toastManager;
@@ -39,6 +59,7 @@ public sealed partial class TrainingSchedulesViewModel : ViewModelBase, INavigab
         _addTrainingScheduleDialogCardViewModel = addTrainingScheduleDialogCardViewModel;
         
         LoadSampleData();
+        UpdateScheduledPeopleCounts();
     }
 
     public TrainingSchedulesViewModel()
@@ -49,89 +70,100 @@ public sealed partial class TrainingSchedulesViewModel : ViewModelBase, INavigab
         _addTrainingScheduleDialogCardViewModel = new AddTrainingScheduleDialogCardViewModel();
         
         LoadSampleData();
+        UpdateScheduledPeopleCounts();
     }
     
     [AvaloniaHotReload]
     public void Initialize()
     {
+        if (IsInitialized) return;
+        LoadSampleData();
+        UpdateScheduledPeopleCounts();
+        IsInitialized = true;
     }
 
     private void LoadSampleData()
     {
         var scheduledClients = CreateSampleData();
+        OriginalScheduledPeople =  scheduledClients;
+        FilterDataByDate(SelectedDate);
+        
+        /*
         var sampleConvertedToCollection = new DataGridCollectionView(scheduledClients);
         ScheduledClients = sampleConvertedToCollection;
+        */
     }
 
     private List<ScheduledPerson> CreateSampleData()
     {
+        var today = DateTime.Today;
         return
         [
             new ScheduledPerson
             {
                 ID = 1001, Picture = "avares://AHON_TRACK/Assets/MainWindowView/user-admin.png", FirstName = "Rome", 
-                LastName = "Calubayan", ContactNumber = "09182736273", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = DateTime.Now, 
-                ScheduledTimeStart = DateTime.Today.AddHours(9), ScheduledTimeEnd = DateTime.Today.AddHours(12), Attendance = null
+                LastName = "Calubayan", ContactNumber = "09182736273", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = today, 
+                ScheduledTimeStart = today.AddHours(1), ScheduledTimeEnd = today.AddHours(2), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1002, Picture = "avares://AHON_TRACK/Assets/MainWindowView/user-admin.png", FirstName = "Sianrey", 
-                LastName = "Flora", ContactNumber = "09198656372", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(8), ScheduledTimeEnd = DateTime.Today.AddHours(11), Attendance = null
+                LastName = "Flora", ContactNumber = "09198656372", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = today,
+                ScheduledTimeStart = today.AddHours(11), ScheduledTimeEnd = today.AddHours(12), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1003, Picture = "", FirstName = "Mardie", 
-                LastName = "Dela Cruz", ContactNumber = "09138545322", PackageType = "Muay Thai", AssignedCoach = "Coach Jedd", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(14), ScheduledTimeEnd = DateTime.Today.AddHours(16),  Attendance = null
+                LastName = "Dela Cruz", ContactNumber = "09138545322", PackageType = "Muay Thai", AssignedCoach = "Coach Jedd", ScheduledDate = today,
+                ScheduledTimeStart = today.AddHours(11), ScheduledTimeEnd = today.AddHours(12),  Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1004, Picture = "avares://AHON_TRACK/Assets/MainWindowView/user-admin.png", FirstName = "JL", 
-                LastName = "Taberdo", ContactNumber = "09237645212", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(14), ScheduledTimeEnd = DateTime.Today.AddHours(16), Attendance = null
+                LastName = "Taberdo", ContactNumber = "09237645212", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = today.AddDays(1),
+                ScheduledTimeStart = today.AddHours(14), ScheduledTimeEnd = today.AddHours(16), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1005, Picture = "", FirstName = "Jav", 
-                LastName = "Agustin", ContactNumber = "09686643211", PackageType = "Muay Thai", AssignedCoach = "Coach Jedd", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(14), ScheduledTimeEnd = DateTime.Today.AddHours(16), Attendance = null
+                LastName = "Agustin", ContactNumber = "09686643211", PackageType = "Muay Thai", AssignedCoach = "Coach Jedd", ScheduledDate = today.AddDays(1),
+                ScheduledTimeStart = today.AddHours(15), ScheduledTimeEnd = today.AddHours(16), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1006, Picture = "", FirstName = "Dave", 
-                LastName = "Dapitillo", ContactNumber = "09676544212", PackageType = "Muay Thai", AssignedCoach = "Coach Jedd", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(17), ScheduledTimeEnd = DateTime.Today.AddHours(18), Attendance = null
+                LastName = "Dapitillo", ContactNumber = "09676544212", PackageType = "Muay Thai", AssignedCoach = "Coach Jedd", ScheduledDate = today.AddDays(2),
+                ScheduledTimeStart = today.AddHours(17), ScheduledTimeEnd = today.AddHours(18), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1007, Picture = "", FirstName = "Daniel", 
-                LastName = "Empinado", ContactNumber = "09666452211", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(17), ScheduledTimeEnd = DateTime.Today.AddHours(18), Attendance = null
+                LastName = "Empinado", ContactNumber = "09666452211", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = today.AddDays(2),
+                ScheduledTimeStart = today.AddHours(17), ScheduledTimeEnd = today.AddHours(18), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1008, Picture = "", FirstName = "Marc", 
-                LastName = "Torres", ContactNumber = "098273647382", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(17), ScheduledTimeEnd = DateTime.Today.AddHours(18), Attendance = null
+                LastName = "Torres", ContactNumber = "098273647382", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = today.AddDays(2),
+                ScheduledTimeStart = today.AddHours(17), ScheduledTimeEnd = today.AddHours(18), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1009, Picture = "", FirstName = "Mark", 
-                LastName = "Dela Cruz", ContactNumber = "091827362837", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(19), ScheduledTimeEnd = DateTime.Today.AddHours(20),  Attendance = null
+                LastName = "Dela Cruz", ContactNumber = "091827362837", PackageType = "Crossfit", AssignedCoach = "Coach Rey", ScheduledDate = today.AddDays(3),
+                ScheduledTimeStart = today.AddHours(19), ScheduledTimeEnd = today.AddHours(20),  Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1010, Picture = "", FirstName = "Adriel", 
-                LastName = "Del Rosario", ContactNumber = "09182837748", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(19), ScheduledTimeEnd = DateTime.Today.AddHours(20), Attendance = null
+                LastName = "Del Rosario", ContactNumber = "09182837748", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = today.AddDays(3),
+                ScheduledTimeStart = today.AddHours(19), ScheduledTimeEnd = today.AddHours(20), Attendance = null
             },
             new ScheduledPerson
             {
                 ID = 1011, Picture = "", FirstName = "JC", 
-                LastName = "Casidor", ContactNumber = "09192818827", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = DateTime.Now,
-                ScheduledTimeStart = DateTime.Today.AddHours(21), ScheduledTimeEnd = DateTime.Today.AddHours(22), Attendance = null
+                LastName = "Casidor", ContactNumber = "09192818827", PackageType = "Boxing", AssignedCoach = "Coach Jho", ScheduledDate = today.AddDays(3),
+                ScheduledTimeStart = today.AddHours(21), ScheduledTimeEnd = today.AddHours(22), Attendance = null
             }
         ];
     }
@@ -177,6 +209,45 @@ public sealed partial class TrainingSchedulesViewModel : ViewModelBase, INavigab
             .DismissOnClick()
             .ShowSuccess();
     }
+
+    private void FilterDataByDate(DateTime selectedDate)
+    {
+        var filteredScheduledData = OriginalScheduledPeople 
+            .Where(w => w.ScheduledDate?.Date == selectedDate.Date)
+            .ToList();
+        
+        CurrentScheduledPeople = filteredScheduledData;
+        ScheduledPeople.Clear();
+        
+        foreach (var schedule in filteredScheduledData)
+        {
+            schedule.PropertyChanged -= OnScheduledChanged;
+            schedule.PropertyChanged += OnScheduledChanged;
+            ScheduledPeople.Add(schedule);
+        }
+        UpdateScheduledPeopleCounts();
+    }
+    
+    private void OnScheduledChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MemberPerson.IsSelected))
+        {
+            UpdateScheduledPeopleCounts();
+        }
+    }
+    
+    partial void OnSelectedDateChanged(DateTime value)
+    {
+        FilterDataByDate(value);
+    }
+    
+    private void UpdateScheduledPeopleCounts()
+    {
+        SelectedCount = ScheduledPeople.Count(x => x.IsSelected);
+        TotalCount = ScheduledPeople.Count;
+
+        SelectAll = ScheduledPeople.Count > 0 && ScheduledPeople.All(x => x.IsSelected);
+    }
 }
 
 public partial class ScheduledPerson : ObservableObject
@@ -211,8 +282,10 @@ public partial class ScheduledPerson : ObservableObject
     [ObservableProperty] 
     private DateTime? _scheduledTimeEnd;
 
-    [ObservableProperty] 
-    private DateTime _scheduledDate;
+    public DateTime? ScheduledDate { get; set; }
+
+    [ObservableProperty]
+    private bool _isSelected;
     
     // public string DateFormatted => ScheduledTime?.ToString("h:mm tt") ?? string.Empty;
     
