f9220210cd06991b773341bc4831a409af4f0948
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-09-19 12:07:07 +0800

    refactor(product-purchase): replace product-filtering with something more simple. Add a purchase-summary section

--- ViewModels/ProductPurchaseViewModel.cs
+++ ViewModels/ProductPurchaseViewModel.cs
@@ -1,30 +1,41 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using HotAvalonia;
 using ShadUI;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AHON_TRACK.ViewModels;
 
 [Page("product-purchase")]
 public sealed partial class ProductPurchaseViewModel : ViewModelBase, INavigable, INotifyPropertyChanged
 {
+    [ObservableProperty] 
+    private string[] _productFilterItems = ["Products", "Gym Packages"];
+
+    [ObservableProperty] 
+    private string _selectedProductFilterItem = "Products";
+    
+    [ObservableProperty] 
+    private string[] _customerTypeFilterItems = ["All", "Walk-in", "Gym Member"];
+
+    [ObservableProperty] 
+    private string _selectedCustomerTypeFilterItem = "All";
+    
     private readonly DialogManager _dialogManager;
     private readonly ToastManager _toastManager;
     private readonly PageManager _pageManager;
-
-    // Backing fields for toggle states
-    private bool _isAllItemsChecked;
-    private bool _isDrinksChecked;
-    private bool _isSupplementsChecked;
-    private bool _isApparelChecked;
-    private bool _isProductsChecked;
+    
+    [ObservableProperty]
+    private ObservableCollection<Customer> _customerList = [];
 
     public ProductPurchaseViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager)
     {
         _dialogManager = dialogManager;
         _toastManager = toastManager;
         _pageManager = pageManager;
+        
+        LoadCustomerList();
     }
 
     public ProductPurchaseViewModel()
@@ -32,95 +43,111 @@ public sealed partial class ProductPurchaseViewModel : ViewModelBase, INavigable
         _dialogManager = new DialogManager();
         _toastManager = new ToastManager();
         _pageManager = new PageManager(new ServiceProvider());
+        
+        LoadCustomerList();
     }
 
     [AvaloniaHotReload]
     public void Initialize()
     {
+        LoadCustomerList();
     }
     
-    public bool IsAllItemsChecked 
+    private void LoadCustomerList()
     {
-        get => _isAllItemsChecked;
-        set
-        {
-            if (!SetField(ref _isAllItemsChecked, value)) return;
-            if (!value) return;
-            IsDrinksChecked = false;
-            IsSupplementsChecked = false;
-            IsApparelChecked = false;
-            IsProductsChecked = false;
-        }
+        var customers = GetCustomerData();
+        CustomerList = new ObservableCollection<Customer>(customers);
     }
 
-    public bool IsDrinksChecked 
+    private List<Customer> GetCustomerData()
     {
-        get => _isDrinksChecked;
-        set
-        {
-            if (!SetField(ref _isDrinksChecked, value)) return;
-            if (!value) return;
-            IsAllItemsChecked = false;
-            IsSupplementsChecked = false;
-            IsApparelChecked = false;
-            IsProductsChecked = false;
-        }
+        return 
+        [
+            new Customer
+            {
+                ID = 1001,
+                FirstName = "Robert Xyz",
+                LastName = "Lucas",
+                CustomerType = "Gym Member"
+            },
+            new Customer
+            {
+                ID = 1002,
+                FirstName = "Sianrey",
+                LastName = "Flora",
+                CustomerType = "Gym Member"
+            },
+            new Customer
+            {
+                ID = 1003,
+                FirstName = "Mardie",
+                LastName = "Dela Cruz",
+                CustomerType = "Walk-in"
+            },
+            new Customer
+            {
+                ID = 1004,
+                FirstName = "Mark",
+                LastName = "Dela Cruz",
+                CustomerType = "Gym Member"
+            },
+            new Customer
+            {
+                ID = 1005,
+                FirstName = "John Carlo",
+                LastName = "Casidor",
+                CustomerType = "Walk-in"
+            },
+            new Customer
+            {
+                ID = 1006,
+                FirstName = "Marc",
+                LastName = "Torres",
+                CustomerType = "Gym Member"
+            },
+            new Customer
+            {
+                ID = 1007,
+                FirstName = "John Maverick",
+                LastName = "Lim",
+                CustomerType = "Gym Member"
+            },
+            new Customer
+            {
+                ID = 1008,
+                FirstName = "Jav",
+                LastName = "Agustin",
+                CustomerType = "Walk-in"
+            },
+            new Customer
+            {
+                ID = 1009,
+                FirstName = "Adriel",
+                LastName = "Del Rosario",
+                CustomerType = "Walk-in"
+            },
+            new Customer
+            {
+                ID = 1010,
+                FirstName = "Uriel Simon",
+                LastName = "Rivera",
+                CustomerType = "Gym Member"
+            }
+        ];
     }
+}
+
+public partial class Customer : ObservableObject
+{
+    [ObservableProperty] 
+    private int? _iD;
+
+    [ObservableProperty] 
+    private string? _firstName = string.Empty;
     
-    public bool IsSupplementsChecked 
-    {
-        get => _isSupplementsChecked;
-        set
-        {
-            if (!SetField(ref _isSupplementsChecked, value)) return;
-            if (!value) return;
-            IsAllItemsChecked = false;
-            IsDrinksChecked = false;
-            IsApparelChecked = false;
-            IsProductsChecked = false;
-        }
-    }
-
-    public bool IsApparelChecked 
-    {
-        get => _isApparelChecked;
-        set
-        {
-            if (!SetField(ref _isApparelChecked, value)) return;
-            if (!value) return;
-            IsAllItemsChecked = false;
-            IsDrinksChecked = false;
-            IsSupplementsChecked = false;
-            IsProductsChecked = false;
-        }
-    }
+    [ObservableProperty] 
+    private string? _lastName = string.Empty;
     
-    public bool IsProductsChecked 
-    {
-        get => _isProductsChecked;
-        set
-        {
-            if (!SetField(ref _isProductsChecked, value)) return;
-            if (!value) return;
-            IsAllItemsChecked = false;
-            IsDrinksChecked = false;
-            IsSupplementsChecked = false;
-            IsApparelChecked = false;
-        }
-    }
-    
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected new void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
+    [ObservableProperty] 
+    private string? _customerType = string.Empty;
 }
