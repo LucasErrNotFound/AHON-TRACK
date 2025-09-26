a56af892fa40e92aec11d2d5e8f6824bdf1f3ebb
Jedd <calubayanromejeddu@gmail.com>
2025-09-26 15:04:43 +0800

    Created a model for Equipment

---
+++ Models/EquipmentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHON_TRACK.Models
+{
+    public class EquipmentModel
+    {
+        public string EquipmentID { get; set; }
+        public string EquipmentName { get; set; }
+        public string Category { get; set; }
+        public int CurrentStock { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public string Supplier { get; set; }
+        public DateTime? WarrantyExpiry { get; set; }
+        public string Condition { get; set; }
+        public string Status { get; set; }
+        public DateTime? LastMaintenance { get; set; }
+        public DateTime? NextMaintenance { get; set; }
+    }
+}
