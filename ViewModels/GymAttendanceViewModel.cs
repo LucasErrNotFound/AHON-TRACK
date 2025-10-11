63fab301b9b5a00526f57cff33eaf58c4bd0b02f
LucasErrNotFound <robertxlucas21@protonmail.com>
2025-10-11 18:54:27 +0800

    feat: add GymAttendance page to project

---
+++ ViewModels/GymAttendanceViewModel.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel;
+using HotAvalonia;
+using ShadUI;
+
+namespace AHON_TRACK.ViewModels;
+
+public class GymAttendanceViewModel : ViewModelBase, INavigable, INotifyPropertyChanged
+{
+    private readonly DialogManager _dialogManager;
+    private readonly ToastManager _toastManager;
+    private readonly PageManager _pageManager;
+
+    public GymAttendanceViewModel(DialogManager dialogManager, ToastManager toastManager, PageManager pageManager)
+    {
+        _dialogManager = dialogManager;
+        _toastManager = toastManager;
+        _pageManager = pageManager;
+    }
+
+    public GymAttendanceViewModel()
+    {
+        _dialogManager =  new DialogManager();
+        _toastManager = new ToastManager();
+        _pageManager = new PageManager(new ServiceProvider());
+    }
+
+    [AvaloniaHotReload]
+    public void Initialize()
+    {
+    }
+}
