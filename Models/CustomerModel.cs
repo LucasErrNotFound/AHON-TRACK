91ddeed37209c5c4fe06e32a25d8ae37cd2d98fb
Jedd <calubayanromejeddu@gmail.com>
2025-10-16 17:35:18 +0800

    added customer model

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using LiveChartsCore.SkiaSharpView.Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHON_TRACK.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;  }
+        public string? CustomerType { get; set; }
+
+    }
+}
